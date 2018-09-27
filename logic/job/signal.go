5838c42939863e9f4b69da68a1ca93a8ee4d81b0
wanjineng <wanjineng@dingtalk.com>
2018-09-27 12:09:28 +0800

    s2

---
+++ logic/job/signal.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"os"
+	"os/signal"
+	"syscall"
+
+	log "github.com/thinkboy/log4go"
+)
+
+// InitSignal register signals handler.
+func InitSignal() {
+	c := make(chan os.Signal, 1)
+	// syscall.SIGSTOP
+	signal.Notify(c, syscall.SIGHUP, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT)
+	for {
+		s := <-c
+		log.Info("job[%s] get a signal %s", Ver, s.String())
+		switch s {
+		case syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT:
+			return
+		case syscall.SIGHUP:
+			continue
+		default:
+			return
+		}
+	}
+}
